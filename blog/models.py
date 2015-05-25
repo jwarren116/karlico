82144ac5abb4452ba91bb83e79943fbefa6243fc
James Warren <jwarren116@gmail.com>
2015-05-25 10:54:09 -0700

    Outline models for blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField('Title', max_length=63)
+    content = models.TextField('Content')
+    created = models.DateTimeField('Created', auto_now_add=True, auto_now=False)
+    display = models.BooleanField('Display on Blog?', default=False)
+
+    def __unicode__(self):
+        return self.title
+
+
+class Image(models.Model):
+    post = models.ForeignKey(BlogPost, related_name='images')
+    image = models.ImageField('Image', upload_to='img')
+    caption = models.TextField('Caption')
+
+    def __unicode__(self):
+        return self.caption
