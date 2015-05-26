4c61e6d1943459b9e9ac744be206301f065c875a
James Warren <jwarren116@gmail.com>
2015-05-25 17:08:28 -0700

    Add class based view for main homepage to display all posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
+from blog.views import IndexView
 
 
 urlpatterns = patterns('',
-    # url(r'^$', 'blog.views.index', name='index'),
+    url(r'^$', IndexView.as_view()),
 )
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from blog.models import BlogPost
 
-# Create your views here.
+
+class IndexView(ListView):
+    model = BlogPost
+    context_object_name = 'posts'
+    template_name = 'blog/index.html'
