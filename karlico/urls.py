1d494769f39235ed5c00a880a5bc9a504b4261cd
James Warren <jwarren116@gmail.com>
2015-05-25 17:10:01 -0700

    Fix URL route in root URL conf

--- karlico/urls.py
+++ karlico/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^blog/', include('blog.urls')),
+    url(r'^$', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
