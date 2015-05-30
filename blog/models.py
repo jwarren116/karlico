30a187359727322e675bfe243afe69cc84b39aca
James Warren <jwarren116@gmail.com>
2015-05-30 11:50:11 -0700

    Outline Category model (FK) for BlogPost objects

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 
+class Category(models.Model):
+    category = models.CharField('Category', max_length=63)
+
+    def __unicode__(self):
+        return self.category
+
+
 class BlogPost(models.Model):
     title = models.CharField('Title', max_length=63)
     content = models.TextField('Content')
+    category = models.ForeignKey(Category, related_name='post')
     created = models.DateTimeField('Created', auto_now_add=True, auto_now=False)
     display = models.BooleanField('Display on Blog?', default=False)
 
