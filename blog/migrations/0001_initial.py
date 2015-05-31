02474ffa1ac0fff69b1a125ba568e8f253176ad5
James Warren <jwarren116@gmail.com>
2015-05-31 11:53:40 -0700

    Add initial migrations

---
+++ blog/migrations/0001_initial.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='BlogPost',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(unique=True, max_length=127, verbose_name=b'Title')),
+                ('slug', models.SlugField(unique=True, max_length=127, verbose_name=b'URL Slug')),
+                ('content', models.TextField(verbose_name=b'Content')),
+                ('created', models.DateTimeField(auto_now_add=True, verbose_name=b'Created')),
+                ('display', models.BooleanField(default=False, verbose_name=b'Display on Blog?')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('category', models.CharField(unique=True, max_length=63, verbose_name=b'Category')),
+                ('slug', models.SlugField(unique=True, max_length=63, verbose_name=b'URL Slug')),
+            ],
+            options={
+                'verbose_name_plural': 'categories',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Image',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('image', models.ImageField(upload_to=b'img', verbose_name=b'Image')),
+                ('caption', models.TextField(verbose_name=b'Caption')),
+                ('post', models.ForeignKey(to='blog.BlogPost')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='blogpost',
+            name='category',
+            field=models.ForeignKey(to='blog.Category'),
+            preserve_default=True,
+        ),
+    ]
