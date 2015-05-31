5eb156f25231eb89e4c650c477e3398135d05c58
James Warren <jwarren116@gmail.com>
2015-05-31 11:52:25 -0700

    Add CategoryAdmin; add slug to prepopulated field for PostAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -12,6 +12,14 @@ class PostAdmin(admin.ModelAdmin):
     model = BlogPost
     inlines = [ImageInline]
     list_display = ('title', 'category', 'content', 'created', 'display')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = ('category', 'slug')
+    prepopulated_fields = {'slug': ('category',)}
+
 
 admin.site.register(BlogPost, PostAdmin)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
