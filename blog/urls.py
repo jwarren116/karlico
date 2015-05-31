268c599f78bb29108e52a6c138a49097f00c2195
James Warren <jwarren116@gmail.com>
2015-05-31 10:30:53 -0700

    Add category URL route

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     url(r'^$', 'blog.views.index', name='index'),
     url(r'^(?P<pk>\d+)/$', 'blog.views.post_detail', name='post_detail'),
     url(r'^posts/$', PostList.as_view(), name='posts'),
+    url(r'^(?P<category>\w+)/$', 'blog.views.category', name='category'),
 )
