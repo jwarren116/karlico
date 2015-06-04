87b6ca4e4a4acf7dd8b6436ce5072552e2c3db78
James Warren <jwarren116@gmail.com>
2015-06-04 10:38:22 -0700

    Fix regular expressions in URLs to accept dash character in slugs

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^$', 'blog.views.index', name='index'),
     url(r'^posts/$', 'blog.views.posts', name='posts'),
-    url(r'^posts/(?P<slug>\w+)/$', 'blog.views.category', name='category'),
-    url(r'^posts/(?P<category>\w+)/(?P<slug>\w+)/$', 'blog.views.post_detail', name='post_detail'),
+    url(r'^posts/(?P<slug>[-\w]+)/$', 'blog.views.category', name='category'),
+    url(r'^posts/(?P<category>[-\w]+)/(?P<slug>[-\w]+)/$', 'blog.views.post_detail', name='post_detail'),
 )
