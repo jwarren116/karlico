f29768f56cbf8394d39299cb87b27350f7440b71
James Warren <jwarren116@gmail.com>
2015-06-05 12:07:46 -0700

    Add static root to settings

--- karlico/settings.py
+++ karlico/settings.py
@@ -42,6 +42,7 @@ class Base(Settings):
     USE_L10N = True
     USE_TZ = True
 
+    STATIC_ROOT = BASE_DIR + '/public/static/'
     STATIC_URL = '/static/'
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'karlico/static/'),
