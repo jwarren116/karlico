c32a8b5d06859b3787dea0156111dbfebd6af6af
James Warren <jwarren116@gmail.com>
2015-06-06 16:38:18 -0700

    Outline factories needed for testing (About, Category, BlogPost)

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from django.core.urlresolvers import reverse
+from blog.models import BlogPost, About, Category
+from selenium import webdriver
+import factory
 
-# Create your tests here.
+
+class CategoryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Category
+
+    title = 'Cats'
+    slug = 'cats'
+
+
+class PostFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = BlogPost
+
+    title = 'Post Title'
+    slug = 'post-title'
+    content = 'This is a new blog post with some content'
+    category = factory.SubFactory(CategoryFactory)
+    display = True
+
+
+class AboutFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = About
+
+    content = 'Some interesting things about me'
